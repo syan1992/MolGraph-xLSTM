f8fd312d4a42ee106fbc51a2e637e6a1beb0a467
Yan <331817159@qq.com>
2025-02-16 17:34:31 -0500

    Delete autorun_clintox.sh

--- autorun_clintox.sh
+++
@@ -1,4 +0,0 @@
-nice -n 19 python main_test4.py --lr_decay_epochs 300 --lr_decay_rate 0.5 --learning_rate 0.0001 --batch_size 128 --epochs 50 \
-		            --trial 41 --dataset clintox --num_tasks 2 --classification --num_blocks 2 --slstm 0 \
-                --data_dir "/home/UWO/ysun2443/code/trimol_dataset/" --num_gc_layers 7 --power 4 --num_dim 128 --num_experts 8 --num_heads 8
-
